8590793a9d33451b23fd03fcbb6b43e0423d06cf
caiolb2000 <caiolb2000@gmail.com>
2024-08-27 16:03:02 +0800

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,14 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { routes } from './app.routes';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { ServicesComponent } from './pages/services/services.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { CaseStudiesComponent } from './pages/case-studies/case-studies.component';
-import { BlogComponent } from './pages/blog/blog.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(BrowserModule, RouterModule)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(BrowserModule, RouterModule)
+  ]
 };
+
